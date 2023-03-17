71b47d3a9bb84bf3a7c79f879116098088e64bf3
Hanna Obando <hannaobandopk@gmail.com>
2023-03-16 23:56:19 -0500

    Pantalla de editar vehiculo creada

---
+++ app/src/main/java/com/example/safegear/EditarVehiculo.kt
@@ -0,0 +1,11 @@
+package com.example.safegear
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditarVehiculo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editar_vehiculo)
+    }
+}
