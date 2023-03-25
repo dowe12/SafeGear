7742711dbbcb34ad0e8f8bb0a5ec02fbd05cbb09
GABRIELA ESCOBAR JUSPIAN <gabriela.escobar.j@uniautonoma.edu.co>
2023-03-24 22:19:58 -0500

    version Beta 2 RecyclerView

--- app/src/main/java/com/example/safegear/adapter/VehiculoAdapter.kt
+++ app/src/main/java/com/example/safegear/adapter/VehiculoAdapter.kt
@@ -1,4 +1,31 @@
 package com.example.safegear.adapter
 
-class VehiculoAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.safegear.R
+import com.example.safegear.model.VehiculoModel
+
+class VehiculoAdapter: RecyclerView.Adapter<VehiculoViewHolder>() {
+
+    private val vehiculos = listOf(
+        VehiculoModel("AAA-111", "Campero", "ACPM", "AAA-AAA","W","W","W",
+        "W","","","",""),
+        VehiculoModel("BBB-111", "Campero", "ACPM", "BBB-AAA","A","A","A",
+            "A","","","",""),
+        VehiculoModel("CCC-111", "Campero", "ACPM", "CCC-AAA","C","C","C",
+            "C","","","","")
+    )
+
+     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VehiculoViewHolder {
+         val layoutInflater = LayoutInflater.from(parent.context)
+         return VehiculoViewHolder(layoutInflater.inflate(R.layout.item_vehiculo, parent, false))
+     }
+
+    override fun onBindViewHolder(holder: VehiculoViewHolder, position: Int) {
+        val item = vehiculos[position]
+        holder.render(item)
+    }
+
+    override fun getItemCount(): Int = vehiculos.size
 }
--- app/src/main/java/com/example/safegear/adapter/VehiculoViewHolder.kt
+++ app/src/main/java/com/example/safegear/adapter/VehiculoViewHolder.kt
@@ -1,4 +1,22 @@
 package com.example.safegear.adapter
 
-class VehiculoViewHolder {
+import android.content.Intent
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.safegear.EditarVehiculo
+import com.example.safegear.databinding.ItemVehiculoBinding
+import com.example.safegear.model.VehiculoModel
+import java.util.*
+
+class VehiculoViewHolder (view: View): RecyclerView.ViewHolder(view){
+        private val binding = ItemVehiculoBinding.bind(view)
+
+        fun render(vehiculoModel : VehiculoModel){
+            binding.tvPlacaListVehiculo.text = vehiculoModel.placa
+
+            binding.imvVerVehiculo.setOnClickListener{
+                val intent = Intent(binding.root.context, EditarVehiculo::class.java)
+                binding.root.context.startActivity(intent)
+            }
+        }
 }
