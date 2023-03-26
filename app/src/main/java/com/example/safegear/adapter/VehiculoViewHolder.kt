bda3e685b67e5f70c25e627029046bd7890e30a6
Hanna Obando <hannaobandopk@gmail.com>
2023-03-25 19:32:46 -0500

    alert dialog para confirmar eliminacion de vehiculo creado

--- app/src/main/java/com/example/safegear/adapter/VehiculoViewHolder.kt
+++ app/src/main/java/com/example/safegear/adapter/VehiculoViewHolder.kt
@@ -2,10 +2,12 @@ package com.example.safegear.adapter
 
 import android.content.Intent
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.safegear.EditarVehiculo
 import com.example.safegear.databinding.ItemVehiculoBinding
 import com.example.safegear.model.VehiculoModel
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import java.util.*
 
 class VehiculoViewHolder(view:View):RecyclerView.ViewHolder(view) {
@@ -19,5 +21,28 @@ class VehiculoViewHolder(view:View):RecyclerView.ViewHolder(view) {
             intent.putExtra("placa",binding.tvPlacaListVehiculo.text.toString())
             itemView.context.startActivity(intent)
         }
+
+        binding.imvEliminarVehiculo.setOnClickListener{
+            showAlertDialog(vehiculoModel)
+        }
+    }
+
+    private fun showAlertDialog(vehiculoModel: VehiculoModel){
+        MaterialAlertDialogBuilder(itemView.context)
+            .setTitle("Eliminar vehículo")
+            .setMessage("¿Deseas eliminar el vehículo?")
+            .setNegativeButton("No"){dialog, _ ->
+                dialog.dismiss()
+            }
+            .setPositiveButton("Si"){dialog, _ ->
+                //Eliminar vehiculo
+                showConfirmationMessage()
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun showConfirmationMessage(){
+        Toast.makeText(itemView.context, "El vehículo ha sido eliminado", Toast.LENGTH_SHORT).show()
     }
 }
